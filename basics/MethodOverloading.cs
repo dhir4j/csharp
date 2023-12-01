48125c31316d3711fe3d508b483665b8547a01c3
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2023-12-01 22:45:41 +0530

    Method Overloading Demonstration

---
+++ basics/MethodOverloading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class MyFirstProgram()
+{
+    //Method Overloading, methods share same name but different parameters.
+    //Each Method should have Unique Signature, 
+    //Name + parameter = Signature
+    public static void Main()
+    {
+        double num1, num2, num3;
+        num1 = 10;
+        num2 = 3.14;
+        num3 = 0.01;
+        Console.WriteLine(Multiply(num1, num2, num3));
+    }
+    static double Multiply(double x, double y) { 
+        return x + y;
+    }
+
+    static double Multiply(double x, double y, double z)
+    {
+        return x + y + z;
+    }
+}
